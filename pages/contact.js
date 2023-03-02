6f41676a2e7ae3eccfe36d30cf426d4c20e7475f
Gautham <95676034+Stylish-Developer@users.noreply.github.com>
2023-03-02 13:12:07 +0530

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -7,20 +7,7 @@ const Contact = () => {
       <Grid container>
         <Grid item m={5} d>
           <Typography variant="h6">
-            Infinijith Apps & Technologies Private Limited.
-            <br />
-            Business Contact : Manjula G.T.
-            <br />
-            Mobile: +91 9585015999.
-            <br />
-            Email: manju@infinijith.com.
-            <br />
-            https: www.infinijith.com.
-            <br />
-            LinkedIn: Infinijith Technologies.
-            <br />
-            Facebook: www.facebook.com/InfinijithTechnologies
-            <br />
+          EMAILID: gowthamsgobi@gmail.com
           </Typography>
         </Grid>
       </Grid>
