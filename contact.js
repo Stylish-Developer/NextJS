ef879ec0718af58342872953d091c908aa8c5409
Gautham <95676034+Stylish-Developer@users.noreply.github.com>
2023-03-02 13:11:12 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Typography, Grid } from "@mui/material";
+
+const Contact = () => {
+  return (
+    <>
+      <Grid container>
+        <Grid item m={5} d>
+          <Typography variant="h6">
+           EMAIL: gowthamsgobi@gmail.com
+          </Typography>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Contact;
